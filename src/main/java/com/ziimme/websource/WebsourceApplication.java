a4832c7eac62d0f9f65f71591671934295871327
Nathaphong.ice <nathaphong.ice@betimes.biz>
2022-11-17 09:12:25 +0700

    first commit java sprint-boot version 8

---
+++ src/main/java/com/ziimme/websource/WebsourceApplication.java
@@ -0,0 +1,13 @@
+package com.ziimme.websource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsourceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ziimme/websource/WebsourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ziimme.websource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
